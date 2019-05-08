94bced4dc71aa4fc0ad407a7b9fc172351f5d05f
benlin131020 <35870749+benlin131020@users.noreply.github.com>
2019-05-09 01:35:29 +0800

    Improving Tensorboard.

--- hw3.py
+++ hw3.py
@@ -1,6 +1,7 @@
 from time import time
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.keras import backend as K
 from data.cifar10 import load_cifar10
 
 NUM_INPUT = 32 * 32 * 3
@@ -9,8 +10,11 @@ NUM_TRAIN_DATA = 50000
 NUM_TEST_DATA = 10000
 BATCH_SIZE = 100
 NUM_BATCHES = NUM_TRAIN_DATA // BATCH_SIZE
-EPOCHS = 10
+EPOCHS = 100
 LEARNING_RATE = 0.0001
+#fp16
+K.set_floatx('float16')
+K.set_epsilon(1e-4)
 
 def weight(shape):
     return tf.Variable(tf.truncated_normal(shape, stddev=0.1), name='W')
@@ -40,31 +44,39 @@ if __name__ == "__main__":
     with tf.name_scope("MaxPool1"):
         pool1 = max_pool_2x2(conv1)    
     with tf.name_scope("Conv2"):
-        w2 = weight([5, 5, 16, 36])
-        b2 = bias([36])
+        w2 = weight([5, 5, 16, 32])
+        b2 = bias([32])
         conv2 = tf.nn.relu(conv2d(pool1, w2) + b2)
     with tf.name_scope("MaxPool2"):
         pool2 = max_pool_2x2(conv2)
     with tf.name_scope("Flatten"):
         flat1 = tf.layers.Flatten()(pool2)
     with tf.name_scope("FC1"):
-        w3 = weight([int(flat1.shape[1]), 128])
         #w3 = weight([2304, 128])
+        w3 = weight([int(flat1.shape[1]), 128])
         b3 = bias([128])
         hidden1 = tf.nn.relu(tf.matmul(flat1, w3) + b3)
-        dropout1 = tf.nn.dropout(hidden1, rate=0.2)
+    with tf.name_scope("FC2"):
+        w4 = weight([128, 256])
+        b4 = bias([256])
+        hidden2 = tf.nn.relu(tf.matmul(hidden1, w4) + b4)
+        dropout1 = tf.nn.dropout(hidden2, rate=0.2)
     with tf.name_scope("OutputLayer"):
-        w4 = weight([128, 10])
+        w4 = weight([256, 10])
         b4 = bias([10])
-        y_predict = tf.matmul(dropout1, w4) + b4
+        logits = tf.matmul(dropout1, w4) + b4
+        y_predict = tf.nn.softmax(logits)
+    with tf.name_scope("Loss"):
+        cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y_label))
+        tf.summary.scalar("cross entropy", cross_entropy_loss)
     with tf.name_scope("Optimizer"):
         #WARNING: This op expects unscaled logits, since it performs a softmax on logits internally for efficiency.
         #Do not call this op with the output of softmax, as it will produce incorrect results.
-        cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y_predict, labels=y_label))
         optimizer = tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(cross_entropy_loss)
-    with tf.name_scope("Evaluation"):
+    with tf.name_scope("Accuracy"):
         correct_prediction = tf.equal(tf.argmax(y_predict, 1), tf.argmax(y_label, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) 
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        tf.summary.scalar("accuracy", accuracy)
     #training
     epoch_list=[]
     accuracy_list=[]
@@ -73,12 +85,18 @@ if __name__ == "__main__":
     startTime=time()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+        train_writer = tf.summary.FileWriter("logs/train", sess.graph)
+        val_writer = tf.summary.FileWriter("logs/val")
+        merged = tf.summary.merge_all()
         sess.run(iter.initializer, feed_dict={x: x_train, y: y_train, batch_size: BATCH_SIZE})
         for epoch in range(EPOCHS):
             for i in range(NUM_BATCHES):
                 sess.run(optimizer)
             
-            loss, acc = sess.run([cross_entropy_loss, accuracy], feed_dict={features: x_val, y_label: y_val})
+            loss, acc, val_result = sess.run([cross_entropy_loss, accuracy, merged], feed_dict={features: x_val, y_label: y_val})
+            train_result = sess.run(merged, feed_dict={features: x_train, y_label: y_train})
+            val_writer.add_summary(val_result, epoch)
+            train_writer.add_summary(train_result, epoch)
             print("Train Epoch:%02d" % (epoch+1), "Loss={:.9f}".format(loss), "Accuracy=", acc)
 
         duration =time()-startTime
@@ -86,5 +104,4 @@ if __name__ == "__main__":
         #evalute model
         sess.run(iter.initializer, feed_dict={x: x_test, y: y_test, batch_size: NUM_TEST_DATA})
         print("Accuracy:", sess.run(accuracy))
-        writer = tf.summary.FileWriter("logs/", sess.graph)
-        
+    
